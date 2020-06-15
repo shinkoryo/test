57714adf8fcd5b551c75d4a3739ddec03424bb09
shinkoryo <49625444+shinkoryo@users.noreply.github.com>
2020-06-16 07:27:02 +0900

    Update ast.py

--- ast.py
+++ ast.py
@@ -103,3 +103,53 @@ yml_lod = yml.load(string)
 
 with open('out_ruamel.yml', 'w', encoding = 'utf-8') as stream:
     yml.dump(yml_lod, stream=stream)
+    
+###################################
+###################################
+###################################
+
+import ast
+import json
+import yaml
+import ruamel.yaml
+
+def remove_str(lst, *args):
+    for arg in args:
+        lst =  [key for key in lst if arg not in key]
+    return lst
+
+def insert_name(lst):
+    i = 0
+    string = '- !!python/object/apply:_ast.'
+    name_list = [(i, v.replace(string, '  ') + ':') for i, v in enumerate(lst) if string in v]
+    
+    for index_no, value in name_list:
+        lst.insert(index_no + i + 1, value)
+        i += 1
+    return lst
+
+def replace_str(lst, *args):
+    for k, v in args:
+        lst =  [(s + ':').replace(k, v) if k in s else s.replace(k, v) for s in lst]
+    return lst   
+
+
+tree = ast.parse(Monster)
+string = '!!python/object/apply:_ast.Module\n  body:\n'
+
+dump = yaml.dump(tree)
+lst = dump.splitlines()
+lst = remove_str(lst, 'ctx:', 'state:', 'col_offset:', 'level:', 'lineno:', 'op: ')
+lst = insert_name(lst)
+
+string = '\n'.join(lst)
+yml = ruamel.yaml.YAML()
+
+output_dict = json.loads(json.dumps(yml.load(string)))
+
+with open('out_yaml.yml', 'w', encoding = 'utf-8') as stream:
+    yml.dump(output_dict, stream)
+    
+    
+    
+    
